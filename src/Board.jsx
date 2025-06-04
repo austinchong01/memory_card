e95fcd93e216381aaf1e9eee4e198c98204236d4
Austin <austin.inhyuk.chong@gmail.com>
2025-06-04 11:09:08 -0700

    Shuffle on Click

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,41 +1,72 @@
 import React, { useState, useEffect } from "react";
 import "./Board.css";
 
-function Card({pokemon}) {
+function Card({ pokemon }) {
   const [data, setData] = useState(null);
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
       .then((response) => {
-        if(!response.ok){
-          throw new Error("Could not fetch resource")
+        if (!response.ok) {
+          throw new Error("Could not fetch resource");
         }
         return response.json();
       })
-      .then((d) => {
-        const sprite = React.createElement("img", {src: `${d.sprites.front_default}`}, null);
-        setData(sprite)
-        setName(d.name)
-      })
+    .then((d) => {
+      setData(d.sprites.front_default);
+      setName(d.name);
+    })
 
-      .catch(error => console.log(error))
+      .catch((error) => console.log(error));
   }, []);
 
   return (
-    <div className="card">
-        <h2>{name}</h2>{data}
-    </div>
-    );
+  <div className="card">
+    <h2>{name}</h2>
+    {data && <img src={data} alt={name} />}
+  </div>
+  );
+}
+
+function shuffle(array) {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
 }
 
 export default function Board() {
+  const [list, setList] = useState([
+    "mew",
+    "mewtwo",
+    "pikachu",
+    "charizard",
+    "blastoise",
+    "venusaur",
+    "lugia",
+    "ho-oh",
+    "arceus",
+    "palkia",
+    "dialga",
+    "flygon",
+  ])
+
+  const handleClick = () => {
+    setList(shuffle(list));
+  };
+
   return (
     <div className="board">
-      <Card pokemon={"bulbasaur"}/>
-      <Card pokemon={"pikachu"}/>
-      <Card pokemon={"blastoise"}/>
-      <Card pokemon={"charizard"}/>
+      {list.map((pokemon) => {
+        return (
+          <div onClick={handleClick} key={pokemon}>
+            <Card pokemon={pokemon}/>
+          </div>
+        );
+      })}
     </div>
   );
 }
